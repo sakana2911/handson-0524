a362aa965da6848f9ba7e2ff7933b401c1186bd7
sakana2911 <56302215+sakana2911@users.noreply.github.com>
2022-05-23 19:22:08 +0900

    Update main.tf

--- Step1/main.tf
+++ Step1/main.tf
@@ -17,23 +17,22 @@ provider "aci" {
   insecure = true
 }
 
-
 resource "aci_tenant" "terraform_tenant" {
   name = "CCOID-1"
 }
 
-resource "aci_vrf" "vrf1" {
+resource "aci_vrf" "vrf" {
   tenant_dn = aci_tenant.terraform_tenant.id
   name      = "VRF1"
 }
 
-resource "aci_bridge_domain" "bd1" {
+resource "aci_bridge_domain" "bd" {
   tenant_dn          = aci_tenant.terraform_tenant.id
-  relation_fv_rs_ctx = aci_vrf.vrf1.name
+  relation_fv_rs_ctx = aci_vrf.vrf.id
   name               = "BD1"
 }
 
 resource "aci_subnet" "bd1_subnet" {
-  bridge_domain_dn = aci_bridge_domain.bd1.id
+  parent_dn = aci_bridge_domain.bd.id
   ip               = "172.16.1.254/24"
 }
