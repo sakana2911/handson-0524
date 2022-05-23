6af81f1f1a919d715ae97b072cdb02e99d55955e
sakana2911 <56302215+sakana2911@users.noreply.github.com>
2022-05-23 19:46:04 +0900

    Add files via upload

---
+++ Step4/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "CiscoDevNet/aci"
+      version = "2.2.1"
+    }
+  }
+}
+
+provider "aci" {
+  # cisco-aci user name
+  username = "admin"
+  # cisco-aci password
+  password = "C!Sco_1234"
+  # cisco-aci url
+  url      = "172.16.10.11"
+  insecure = true
+}
+
+# Define an ACI Tenant Resource.
+resource "aci_tenant" "terraform_tenant" {
+  name = "CCOID-4"
+}
+
+# Define an ACI Tenant VRF Resource.
+resource "aci_vrf" "vrf" {
+  tenant_dn = aci_tenant.terraform_tenant.id
+  name      = "VRF4"
+}
+
+# Define an ACI Tenant BD Resource.
+resource "aci_bridge_domain" "bd" {
+  tenant_dn          = aci_tenant.terraform_tenant.id
+  relation_fv_rs_ctx = aci_vrf.vrf.id
+  name               = "BD4"
+}
+
+# Define an ACI Application Profile Resource.
+resource "aci_application_profile" "terraform_ap" {
+    tenant_dn  = aci_tenant.terraform_tenant.id
+    name       = "AP4"
+    description = "App Profile Created Using Terraform"
+}
+
+# Define an ACI Application EPG Resource.
+resource "aci_application_epg" "terraform_epg" {
+    application_profile_dn  = aci_application_profile.terraform_ap.id
+    name                    = "EPG4"
+    relation_fv_rs_bd       = aci_bridge_domain.bd.id
+    description             = "EPG Created Using Terraform"
+}
+
+# Associate the EPG Resources with Domain.
+resource "aci_epg_to_domain" "terraform_epg_domain" {
+    application_epg_dn    = aci_application_epg.terraform_epg.id
+    tdn   = "uni/phys-MGMT_PD"
+}
+
+# Associate the EPG Resource with Static Path
+resource "aci_epg_to_static_path" "terraform_static_path" {
+    application_epg_dn = aci_application_epg.terraform_epg.id
+    tdn = "topology/pod-1/protpaths-101-102/pathep-[HX1_PG]"
+    encap = "vlan-200"
+}
